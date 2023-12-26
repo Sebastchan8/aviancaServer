e06a979d174676f9200a93fc5f7fb2e73bb56d4e
sebastchan <jsalan5133@uta.edu.ec>
2023-12-26 01:47:54 -0500

    authentication and flights added

--- controllers/interactionsController.js
+++ controllers/interactionsController.js
@@ -10,14 +10,19 @@ exports.login = async (req, res) => {
         *
         FROM users
         WHERE email = ?`,
-        [email]
+            [email]
         );
-        
+
         if (user.length === 0) {
             return res.status(401).json({ error: 'Invalid credentials' });
         }
 
         if (user[0].password == password) {
+            // NOTIFICATION AND STATUS VIAJES
+            // const [rows] = await connection.query(
+            //     'UPDATE reservations set status = 1 where  = ?',
+            //     [ticket_id]
+            // );
             return res.json({ message: 'Login successfully!', user: user[0] });
         } else {
             return res.status(401).json({ error: 'Invalid credentials' });
@@ -34,7 +39,7 @@ exports.logout = async (req, res) => {
             if (err) {
                 res.status(500).send('Log out failed');
             } else {
-                res.json({ message: 'Logout successfully!'});
+                res.json({ message: 'Logout successfully!' });
             }
         });
     } catch (error) {
@@ -61,7 +66,8 @@ exports.signup = async (req, res) => {
             'INSERT INTO users (firstname, lastname, phone, address, card, email, password) VALUES (?, ?, ?, ?, ?, ?, ?)',
             [firstname, lastname, phone, address, card, email, password]
         );
-
+        //DEFAULT NOTIFICATION---------------------------------------------
+        ///////////////////////////////
         return res.json(result);
     } catch (error) {
         console.error(error);
@@ -111,6 +117,100 @@ exports.getAerolines = async (req, res) => {
     }
 }
 
+exports.getAvailableFlights = async (req, res) => {
+    try {
+        const { departure_id, destination_id, departure_date, destination_date, adults, children, round } = req.body
+        let query, arguments
+        if (departure_id == destination_id) {
+            query = `
+            SELECT
+                f.flight_id,
+                al.aeroline_name,
+                al.aeroline_img,
+                d.id AS departure_city_id,
+                d.city AS departure_city,
+                f.departure_date,
+                a.id AS destination_city_id,
+                a.city AS destination_city,
+                f.destination_date,
+                f.adult_price,
+                f.child_price,
+                f.adult_available,
+                f.child_available,
+                EXISTS (
+                    SELECT 1
+                    FROM flights r
+                    WHERE r.departure_city_id = f.destination_city_id
+                        AND r.destination_city_id = f.departure_city_id
+                        AND r.departure_date > f.destination_date
+                ) AS round
+            FROM
+                flights f
+                JOIN aerolines al ON f.aeroline_id = al.aeroline_id
+                JOIN cities d ON f.departure_city_id = d.id
+                JOIN cities a ON f.destination_city_id = a.id
+            WHERE
+                f.departure_city_id = ?
+                OR f.destination_city_id = ?`
+            arguments = [departure_id, destination_id]
+        } else {
+            query = `
+                SELECT
+                    f.flight_id,
+                    al.aeroline_name,
+                    al.aeroline_img,
+                    d.id AS departure_city_id,
+                    d.city AS departure_city,
+                    f.departure_date,
+                    a.id AS destination_city_id,
+                    a.city AS destination_city,
+                    f.destination_date,
+                    f.adult_price,
+                    f.child_price,
+                    f.adult_available,
+                    f.child_available,
+                    EXISTS (
+                        SELECT 1
+                        FROM flights r
+                        WHERE r.departure_city_id = f.destination_city_id
+                            AND r.destination_city_id = f.departure_city_id
+                            AND r.departure_date > f.destination_date
+                    ) AS round
+                FROM
+                    flights f
+                    JOIN aerolines al ON f.aeroline_id = al.aeroline_id
+                    JOIN cities d ON f.departure_city_id = d.id
+                    JOIN cities a ON f.destination_city_id = a.id
+                WHERE
+                    f.departure_city_id = ?
+                    AND f.destination_city_id = ?
+                    AND f.departure_date LIKE ?
+                    AND f.adult_available >= ?
+                    AND f.child_available >= ?`
+            arguments = [departure_id, destination_id, `%${departure_date}%`, adults, children]
+        }
+        const [rounded] = await connection.query(`
+            SELECT *
+            FROM flights
+            WHERE departure_city_id = ?
+            AND destination_city_id = ?
+            AND departure_date LIKE ?`,
+            [destination_id, departure_id, `%${destination_date}%`]);
+
+        const [rows] = await connection.query(query, arguments);
+
+        if ((round && rounded.length > 0) || !round) {
+            res.json({ flights: rows, round: round });
+        } else {
+            res.json({ flights: [], round: round });
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Getting flights error!');
+    }
+}
+
 exports.getFlights = async (req, res) => {
     try {
         const [rows] = await connection.query(`
@@ -124,6 +224,103 @@ exports.getFlights = async (req, res) => {
     }
 }
 
+exports.getFlight = async (req, res) => {
+    try {
+        const flight_id = req.params.flight_id
+        const [rows] = await connection.query(`
+        SELECT
+            f.flight_id,
+            al.aeroline_name,
+            al.aeroline_img,
+            d.id AS departure_city_id,
+            d.city AS departure_city,
+            f.departure_date,
+            a.id AS destination_city_id,
+            a.city AS destination_city,
+            f.destination_date,
+            f.adult_price,
+            f.child_price,
+            f.adult_available,
+            f.child_available
+        FROM
+            flights f
+            JOIN aerolines al ON f.aeroline_id = al.aeroline_id
+            JOIN cities d ON f.departure_city_id = d.id
+            JOIN cities a ON f.destination_city_id = a.id
+        WHERE
+            f.flight_id = ?`, [flight_id]);
+        console.log("EL ID: ", rows[0]);
+        res.json(rows[0]);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Getting flights error!');
+    }
+}
+
+exports.getRoundedFlights = async (req, res) => {
+    try {
+        const flight_id = req.params.flight_id
+
+        const [flight] = await connection.query(`
+        SELECT
+            f.flight_id,
+            al.aeroline_name,
+            al.aeroline_img,
+            d.id AS departure_city_id,
+            d.city AS departure_city,
+            f.departure_date,
+            a.id AS destination_city_id,
+            a.city AS destination_city,
+            f.destination_date,
+            f.adult_price,
+            f.child_price,
+            f.adult_available,
+            f.child_available
+        FROM
+            flights f
+            JOIN aerolines al ON f.aeroline_id = al.aeroline_id
+            JOIN cities d ON f.departure_city_id = d.id
+            JOIN cities a ON f.destination_city_id = a.id
+        WHERE
+            f.flight_id = ?`, [flight_id]);
+
+        const { departure_city_id, destination_city_id, destination_date } = flight[0]
+
+        const [rows] = await connection.query(`
+        SELECT
+            f.flight_id,
+            al.aeroline_name,
+            al.aeroline_img,
+            d.id AS departure_city_id,
+            d.city AS departure_city,
+            f.departure_date,
+            a.id AS destination_city_id,
+            a.city AS destination_city,
+            f.destination_date,
+            f.adult_price,
+            f.child_price,
+            f.adult_available,
+            f.child_available
+        FROM
+            flights f
+            JOIN aerolines al ON f.aeroline_id = al.aeroline_id
+            JOIN cities d ON f.departure_city_id = d.id
+            JOIN cities a ON f.destination_city_id = a.id
+        WHERE
+            f.destination_city_id = ?
+            AND f.departure_city_id = ?
+            AND f.departure_date > ?`,
+            [departure_city_id, destination_city_id, destination_date]);
+        res.json(rows);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Getting flights error!');
+    }
+}
+
+// -----------------------
+
+
 exports.getUserData = async (req, res) => {
     try {
         const user_id = req.params.user_id
@@ -133,6 +330,21 @@ exports.getUserData = async (req, res) => {
                 *
             FROM users WHERE user_id = ?`, [user_id]);
         res.json(rows[0]);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Getting flights error!');
+    }
+}
+
+exports.getUserNotifications = async (req, res) => {
+    try {
+        const user_id = req.params.user_id
+
+        const [rows] = await connection.query(`
+            SELECT 
+                *
+            FROM notifications WHERE user_id = ?`, [user_id]);
+        res.json(rows);
     } catch (error) {
         console.log(error);
         res.status(500).send('Getting flights error!');
@@ -156,9 +368,144 @@ exports.updateUserData = async (req, res) => {
         const [rows] = await connection.query(`
             UPDATE users 
             SET firstname = ?, lastname = ?, phone = ?, address = ?, card = ?, email = ?, password = ?
-            WHERE user_id = ?`, 
+            WHERE user_id = ?`,
             [firstname, lastname, phone, address, card, email, password, user_id]);
         res.json(rows[0]);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Getting updated error!');
+    }
+}
+
+exports.getUserTickets = async (req, res) => {
+    try {
+        const user_id = req.params.user_id
+
+        const [all] = await connection.query(
+            'SELECT * FROM reservations WHERE user_id = ?',
+            [user_id]
+        );
+
+        let ticketQuery = `
+        SELECT
+            f.flight_id,
+            al.aeroline_name,
+            al.aeroline_img,
+            d.id AS departure_city_id,
+            d.city AS departure_city,
+            f.departure_date,
+            a.id AS destination_city_id,
+            a.city AS destination_city,
+            f.destination_date,
+            f.adult_price,
+            f.child_price,
+            f.adult_available,
+            f.child_available
+        FROM
+            flights f
+            JOIN aerolines al ON f.aeroline_id = al.aeroline_id
+            JOIN cities d ON f.departure_city_id = d.id
+            JOIN cities a ON f.destination_city_id = a.id
+        WHERE
+            f.flight_id = ?`
+
+        let tickets = []
+
+        for (let i = 0; i < all.length; i++) {
+            let returnTicket = [{}]
+            let [departureTicket] = await connection.query(ticketQuery, [all[i].departure_flight_id])
+            if (all[i].return_flight_id != null) {
+                [returnTicket] = await connection.query(ticketQuery, [all[i].return_flight_id])
+            }
+            tickets.push({
+                "reservation_id": all[i].reservation_id,
+                "user_id": all[i].user_id,
+                "adults": all[i].adults,
+                "children": all[i].children,
+                "total": all[i].total,
+                "status": all[i].status,
+                "departureFlight": departureTicket[0],
+                "returnFlight": returnTicket[0]
+            })
+        }
+
+        res.json(tickets);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Getting updated error!');
+    }
+}
+
+exports.buyTicket = async (req, res) => {
+    try {
+        const user_id = req.params.user_id
+        const { departure_flight_id, return_flight_id, adults, children, total } = req.body
+        console.log("re id: ", return_flight_id)
+
+        const [rows] = await connection.query(
+            'INSERT INTO reservations values (null, ?, ?, ?, ?, ?, ?, 0)',
+            [user_id, departure_flight_id, return_flight_id, adults, children, total]
+        );
+
+        let query, arguments
+        if (return_flight_id) {
+            query = `UPDATE flights 
+                    set adult_available = adult_available - ?,
+                    child_available = child_available - ?
+                    WHERE flight_id IN (?, ?)`
+            arguments = [adults, children, departure_flight_id, return_flight_id]
+        } else {
+            query = `UPDATE flights 
+                    set adult_available = adult_available - ?,
+                    child_available = child_available - ?
+                    WHERE flight_id = ?`
+            arguments = [adults, children, departure_flight_id]
+        }
+        const [updated] = await connection.query(query, arguments);
+        res.json(rows);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Getting updated error!');
+    }
+}
+
+exports.cancelTicket = async (req, res) => {
+    try {
+        const ticket_id = req.params.ticket_id
+
+        const [rows] = await connection.query(
+            'UPDATE reservations set status = -1 where reservation_id = ?',
+            [ticket_id]
+        );
+
+        const [reservation] = await connection.query(
+            'SELECT * from reservations where reservation_id = ?',
+            [ticket_id]
+        );
+
+        let query, arguments,
+            departure_flight_id = reservation[0].departure_flight_id,
+            return_flight_id = reservation[0].return_flight_id,
+            adults = reservation[0].adults,
+            children = reservation[0].children
+
+        if (return_flight_id != null) {
+            query = `UPDATE flights 
+                    set adult_available = adult_available + ?,
+                    child_available = child_available + ?
+                    WHERE flight_id IN (?, ?)`
+            arguments = [adults, children, departure_flight_id, return_flight_id]
+        } else {
+            query = `UPDATE flights 
+                    set adult_available = adult_available + ?,
+                    child_available = child_available + ?
+                    WHERE flight_id = ?`
+            arguments = [adults, children, departure_flight_id]
+        }
+        
+        const [updated] = await connection.query(query, arguments);
+
+        res.json(updated);
     } catch (error) {
         console.log(error);
         res.status(500).send('Getting updated error!');
@@ -189,15 +536,15 @@ exports.getChannel = async (req, res) => {
         const visitorChannelId = req.session.user.channel_id;
 
         let subscription_status;
-        if(channelId == visitorChannelId){
+        if (channelId == visitorChannelId) {
             subscription_status = 0;
-        }else{
+        } else {
             const [sub] = await connection.query(`
                 SELECT * FROM subscribers WHERE channel_id_main = ? AND channel_id_subscriber = ?`,
-            [channelId, visitorChannelId]);
-            if(sub.length === 0){
+                [channelId, visitorChannelId]);
+            if (sub.length === 0) {
                 subscription_status = -1;
-            }else{
+            } else {
                 subscription_status = 1;
             }
 
@@ -212,8 +559,8 @@ exports.getChannel = async (req, res) => {
             LEFT JOIN videos v ON c.channel_id = v.channel_id
             WHERE c.channel_id = ?
             GROUP BY c.channel_id`,
-        [channelId]);
-        res.json([{ ...rows[0], subscription_status}]);
+            [channelId]);
+        res.json([{ ...rows[0], subscription_status }]);
     } catch (error) {
         console.log(error);
         res.status(500).send('Getting channel error!');
--- routes/interactions.route.js
+++ routes/interactions.route.js
@@ -10,10 +10,19 @@ router.post('/signup', interactionsController.signup);
 
 
 router.get('/flights', interactionsController.getFlights);
+router.get('/flights/:flight_id', interactionsController.getFlight);
+router.get('/flights/rounded/:flight_id', interactionsController.getRoundedFlights);
+router.post('/flights', interactionsController.getAvailableFlights);
+
 router.get('/cities', interactionsController.getCities);
 router.get('/aerolines', interactionsController.getAerolines);
 
 router.get('/user/:user_id', interactionsController.getUserData);
 router.put('/user/:user_id', interactionsController.updateUserData);
+router.get('/notifications/:user_id', interactionsController.getUserNotifications);
+
+router.put('/ticket/:user_id', interactionsController.buyTicket);
+router.get('/ticket/:user_id', interactionsController.getUserTickets);
+router.delete('/ticket/:ticket_id', interactionsController.cancelTicket);
 
 module.exports = router;
